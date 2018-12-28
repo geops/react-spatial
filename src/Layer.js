a996ebf1d10441215954fad51014c4b77069fa13
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2018-12-28 18:26:54 +0100

    Add setStyle to Layer class.

--- src/Layer.js
+++ src/Layer.js
@@ -29,6 +29,10 @@ export default class Layer {
     this.olLayer.changed();
   }
 
+  setStyle(style) {
+    this.olLayer.setStyle(style);
+  }
+
   setVisible(visible) {
     this.visible = visible;
     this.olLayer.setVisible(visible);
