1e0e7e2e24d4c62d082e2785c5eb071f82b55464
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-01-07 16:14:44 +0100

    fix formatting

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ],
-  "ignore": [
-    "*.scss"
-  ]
-}
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'],
+  ignore: ['*.scss'],
+};
