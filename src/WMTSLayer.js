6eccf8fc5651371a902af9cd9c57c7eace5d02dd
lucien <lucien.edel@geops.de>
2019-01-31 09:58:41 +0100

    [TRAFSHUL-6] Format with Prettier

--- src/WMTSLayer.js
+++ src/WMTSLayer.js
@@ -3,16 +3,36 @@ import WMTSSource from 'ol/source/WMTS';
 import WMTSTilegrid from 'ol/tilegrid/WMTS';
 import Layer from './Layer';
 
-const wmtsResolutions = [156543.033928, 78271.516964, 39135.758482,
-  19567.879241, 9783.9396205, 4891.96981025, 2445.98490513, 1222.99245256,
-  611.496226281, 305.748113141, 152.87405657, 76.4370282852,
-  38.2185141426, 19.1092570713, 9.55462853565, 4.77731426782,
-  2.38865713391, 1.19432856696, 0.597164283478, 0.298582141739];
+const wmtsResolutions = [
+  156543.033928,
+  78271.516964,
+  39135.758482,
+  19567.879241,
+  9783.9396205,
+  4891.96981025,
+  2445.98490513,
+  1222.99245256,
+  611.496226281,
+  305.748113141,
+  152.87405657,
+  76.4370282852,
+  38.2185141426,
+  19.1092570713,
+  9.55462853565,
+  4.77731426782,
+  2.38865713391,
+  1.19432856696,
+  0.597164283478,
+  0.298582141739,
+];
 
 const wmtsMatrixIds = wmtsResolutions.map((res, i) => `${i}`);
 
 const projectionExtent = [
-  -20037508.3428, -20037508.3428, 20037508.3428, 20037508.3428,
+  -20037508.3428,
+  -20037508.3428,
+  20037508.3428,
+  20037508.3428,
 ];
 
 export default class WMTSLayer extends Layer {
@@ -27,11 +47,13 @@ export default class WMTSLayer extends Layer {
       source: new WMTSSource({
         url,
         requestEncoding: 'REST',
-        tileGrid: tileGrid || new WMTSTilegrid({
-          extent: projectionExtent,
-          resolutions: wmtsResolutions,
-          matrixIds: wmtsMatrixIds,
-        }),
+        tileGrid:
+          tileGrid ||
+          new WMTSTilegrid({
+            extent: projectionExtent,
+            resolutions: wmtsResolutions,
+            matrixIds: wmtsMatrixIds,
+          }),
       }),
     });
 
--- src/WMTSLayer.test.js
+++ src/WMTSLayer.test.js
@@ -2,8 +2,8 @@ import WMTSLayer from './WMTSLayer';
 
 test('WMTSLayer should initialize', () => {
   const layer = new WMTSLayer({
-      name: 'Layer',
-      url: 'tilelayer/{TileMatrix}/{TileCol}/{TileRow}.png',
-    });
+    name: 'Layer',
+    url: 'tilelayer/{TileMatrix}/{TileCol}/{TileRow}.png',
+  });
   expect(layer).toBeInstanceOf(WMTSLayer);
 });
