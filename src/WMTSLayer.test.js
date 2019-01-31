a8253177c2214d3a028ed38f324561653ad54b9c
lucien <lucien.edel@geops.de>
2019-01-31 09:52:21 +0100

    [TRAFSHUL-6] Add test for WMTS layer

---
+++ src/WMTSLayer.test.js
@@ -0,0 +1,9 @@
+import WMTSLayer from './WMTSLayer';
+
+test('WMTSLayer should initialize', () => {
+  const layer = new WMTSLayer({
+      name: 'Layer',
+      url: 'tilelayer/{TileMatrix}/{TileCol}/{TileRow}.png',
+    });
+  expect(layer).toBeInstanceOf(WMTSLayer);
+});
