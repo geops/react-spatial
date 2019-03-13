ce89e93532a4cc1ed44e1b292cd9defca2161d1e
lucien <lucien.edel@geops.de>
2019-03-13 14:08:29 +0100

    Update test for TopicList

--- src/components/TopicList/TopicList.test.js
+++ src/components/TopicList/TopicList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { configure } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-canvas-mock';
 import OLMap from 'ol/Map';
@@ -10,6 +10,18 @@ import ConfigReader from '../../ConfigReader';
 import LayerService from '../../LayerService';
 
 configure({ adapter: new Adapter() });
+
+const mountTopicList = (topics, props) => {
+  const layers = ConfigReader.readConfig(new OLMap({}), topics);
+  const layerService = new LayerService(layers);
+  return mount(
+    <TopicList
+      topics={topics}
+      propsToLayerTree={{ layerService }}
+      {...props || {}}
+    />,
+  );
+};
 
 const renderTopicList = (topics, props) => {
   const layers = ConfigReader.readConfig(
@@ -28,7 +40,9 @@ const renderTopicList = (topics, props) => {
   expect(tree).toMatchSnapshot();
 };
 
-describe('LayerTree', () => {
+const toggleItem = '.tm-topic-list-toggle';
+
+describe('Topiclist', () => {
   describe('matches snapshots', () => {
     test('using default properties.', () => {
       renderTopicList(topicData);
@@ -45,12 +59,108 @@ describe('LayerTree', () => {
 
     test('when classNames are used.', () => {
       renderTopicList(topicData, {
-        className: 'foo',
-        classNameItem: 'bar',
-        classNameInput: 'qux',
-        classNameToggle: 'quux',
-        classNameArrow: 'ged',
+        className: 'mimi',
+        classNameItem: 'mama',
+        classNameInput: 'momo',
+        classNameToggle: 'mumu',
+        classNameArrow: 'meme',
       });
+    });
+  });
+
+  describe('triggers onTopicClick', () => {
+    let wrapper;
+    let spy;
+    let spy2;
+    const topicsData = [
+      {
+        id: 'topic2',
+        name: 'Topic 2',
+        visible: true,
+        expanded: false,
+        children: [],
+      },
+    ];
+
+    const funcs = {
+      topicClick: () => {},
+      topicToggle: () => {},
+    };
+
+    const expectCalled = () => {
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy2).toHaveBeenCalledTimes(0);
+    };
+
+    beforeEach(() => {
+      wrapper = mountTopicList(topicsData, {
+        onTopicClick: () => funcs.topicClick(),
+        onTopicToggle: () => funcs.topicToggle(),
+      });
+      spy = jest.spyOn(funcs, 'topicClick');
+      spy2 = jest.spyOn(funcs, 'topicToggle');
+    });
+
+    afterEach(() => {
+      spy.mockRestore();
+      spy2.mockRestore();
+    });
+
+    test('when we press enter with keyboard on the label element.', () => {
+      wrapper
+        .find('label')
+        .at(0)
+        .simulate('keypress', { which: 13 });
+      expectCalled();
+    });
+
+    test('when we click on input.', () => {
+      wrapper
+        .find('input')
+        .at(0)
+        .simulate('click');
+      expectCalled();
+    });
+  });
+
+  describe('triggers onTopicToggle', () => {
+    let wrapper;
+    let spy;
+    const topicsData = [
+      {
+        id: 'topic2',
+        name: 'Topic 2',
+        visible: true,
+        expanded: true,
+        children: [],
+      },
+    ];
+
+    const funcs = {
+      topicToggle: () => {},
+    };
+
+    const expectCalled = () => {
+      expect(spy).toHaveBeenCalledTimes(1);
+    };
+
+    beforeEach(() => {
+      wrapper = mountTopicList(topicsData, {
+        onTopicToggle: () => funcs.topicToggle(),
+      });
+      spy = jest.spyOn(funcs, 'topicToggle');
+    });
+
+    afterEach(() => {
+      spy.mockRestore();
+    });
+
+    test('when we click on toggle button (label+arrow) of a topic', () => {
+      wrapper
+        .find(toggleItem)
+        .first()
+        .simulate('click');
+      expectCalled();
     });
   });
 });
