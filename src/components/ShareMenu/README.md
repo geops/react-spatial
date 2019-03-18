bea099ee912f331dac3d134ea812b67df76e4851
lucien <lucien.edel@geops.de>
2019-03-18 17:06:50 +0100

    Download map as picture, from ShareMenu

--- src/components/ShareMenu/README.md
+++ src/components/ShareMenu/README.md
@@ -4,7 +4,45 @@ This demonstrates the use of Select.
 
 ```jsx
 import React from  'react'
+import BasicMap from 'react-spatial/components/BasicMap';
+import Layer from 'react-spatial/Layer';
+import TileLayer from 'ol/layer/Tile';
+import TileGrid from 'ol/tilegrid/TileGrid';
+import TileImageSource from 'ol/source/TileImage';
+import { getCenter} from 'ol/extent';
+import ConfigReader from '../../ConfigReader';
 import ShareMenu from 'react-spatial/components/ShareMenu';
+import OLMap from 'ol/Map';
 
-<ShareMenu url={window.location.href}/>;
+class ShareMenuExample extends React.Component {
+   constructor(props) {
+    super(props);
+    this.map = new OLMap({controls:[]});
+    this.center = [-10997148, 4569099];
+
+    const layers = ConfigReader.readConfig(
+      this.map,
+      treeData,
+    );
+  };
+
+render() {
+    return (
+      <div className="tm-share-menu-example">
+        <BasicMap
+          map={this.map}
+          center={this.center}
+          zoom={3}
+          layers={this.layers}
+        />
+        <ShareMenu
+          url={window.location.href}
+          map={this.map}
+        />
+      </div>
+    );
+  }
+}
+
+<ShareMenuExample />;
 ```
--- src/components/ShareMenu/ShareMenu.js
+++ src/components/ShareMenu/ShareMenu.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { TiSocialFacebook, TiSocialTwitter } from 'react-icons/ti';
+import OLMap from 'ol/Map';
+import { TiImage, TiSocialFacebook, TiSocialTwitter } from 'react-icons/ti';
 import { FiMail } from 'react-icons/fi';
 import BlankLink from '../BlankLink';
+import Button from '../Button';
 
 const configPropType = PropTypes.arrayOf(
   PropTypes.shape({
@@ -17,6 +19,7 @@ const propTypes = {
    * Translation function.
    */
   t: PropTypes.func,
+  map: PropTypes.instanceOf(OLMap),
   className: PropTypes.string,
   classNameIcon: PropTypes.string,
   socialShareConfig: configPropType,
@@ -24,6 +27,7 @@ const propTypes = {
 };
 
 const defaultProps = {
+  map: null,
   className: 'tm-share-menu',
   classNameIcon: 'tm-share-menu-icon',
   socialShareConfig: [
@@ -44,38 +48,125 @@ const defaultProps = {
       className: 'ta-mail-icon',
     },
   ],
-  extraSocialShareConfig: [],
+  extraSocialShareConfig: [
+    {
+      title: 'Karte als Bild speichern.',
+      icon: <TiImage focusable={false} />,
+      className: 'ta-image-icon',
+    },
+  ],
   t: t => t,
 };
 
-const ShareMenu = ({
-  className,
-  classNameIcon,
-  socialShareConfig,
-  extraSocialShareConfig,
-  t,
-}) => {
-  const config = [...socialShareConfig, ...extraSocialShareConfig];
-
-  for (let i = 0; i < config.length; i += 1) {
-    config[i].url = config[i].url.replace('{url}', window.location.href);
+class ShareMenu extends PureComponent {
+  static getDownloadImageName() {
+    return window.document.title.replace(/ /g, '_').toLowerCase();
   }
 
-  return (
-    <div className={className}>
-      {config.map(conf => (
-        <div
-          className={`${classNameIcon} ${conf.className || ''}`}
-          key={conf.title}
-        >
-          <BlankLink href={conf.url} title={t(conf.title)}>
-            {conf.icon}
-          </BlankLink>
-        </div>
-      ))}
-    </div>
-  );
-};
+  getCanvasImage(opts, asMSBlob) {
+    const { map } = this.props;
+    let image;
+
+    map.once('postcompose', evt => {
+      const { canvas } = evt.context;
+      const { height, width } = canvas;
+
+      const destinationCanvas = document.createElement('canvas');
+      destinationCanvas.width = width;
+      destinationCanvas.height = height;
+
+      const destContext = destinationCanvas.getContext('2d');
+      destContext.fillStyle = 'white';
+      destContext.fillRect(0, 0, width, height);
+      destContext.drawImage(canvas, 0, 0, width, height, 0, 0, width, height);
+
+      if (asMSBlob) {
+        image = destinationCanvas.msToBlob();
+      } else {
+        image = destinationCanvas.toDataURL(opts.format || 'image/jpeg');
+      }
+    });
+    map.renderSync();
+    return image;
+  }
+
+  downloadCanvasImage(e, options) {
+    const opts = options || { format: 'image/jpeg' };
+    if (/msie (9|10)/gi.test(window.navigator.userAgent.toLowerCase())) {
+      // ie 9 and 10
+      const w = window.open('about:blank', '');
+      w.document.write(
+        `<img src="${this.getCanvasImage(opts)}" alt="from canvas"/>`,
+      );
+    } else if (window.navigator.msSaveBlob) {
+      // ie 11 and higher
+      window.navigator.msSaveBlob(
+        new Blob([this.getCanvasImage(opts, true)], { type: opts.format }),
+        `${ShareMenu.getDownloadImageName()}.jpg`,
+      );
+    } else {
+      const link = document.createElement('a');
+      link.download = `${ShareMenu.getDownloadImageName()}.jpg`;
+      link.href = this.getCanvasImage(opts);
+      link.click();
+    }
+
+    if (window.navigator.msSaveBlob) {
+      // ie only
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+
+  renderImgSaveButton(conf) {
+    return (
+      <Button
+        className="tm-share-button"
+        title="Karte als Bild speichern."
+        onClick={e => this.downloadCanvasImage(e)}
+      >
+        <span>{conf.icon}</span>
+      </Button>
+    );
+  }
+
+  render() {
+    const {
+      className,
+      classNameIcon,
+      socialShareConfig,
+      extraSocialShareConfig,
+      t,
+    } = this.props;
+
+    const config = [...socialShareConfig, ...extraSocialShareConfig];
+
+    for (let i = 0; i < config.length; i += 1) {
+      if (config[i].url) {
+        config[i].url = config[i].url.replace('{url}', window.location.href);
+      }
+    }
+
+    return (
+      <div className={className}>
+        {config.map(conf => (
+          <div
+            className={`${classNameIcon} ${conf.className || ''}`}
+            key={conf.title}
+          >
+            {conf.url ? (
+              <BlankLink href={conf.url} title={t(conf.title)}>
+                {conf.icon}
+              </BlankLink>
+            ) : (
+              this.renderImgSaveButton(conf)
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 ShareMenu.propTypes = propTypes;
 ShareMenu.defaultProps = defaultProps;
