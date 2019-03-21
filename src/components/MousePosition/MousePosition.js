7998366ddd913dd0f075c8210986c63dc2ab539b
oterral <oterral@users.noreply.github.com>
2019-03-21 13:31:19 +0100

    Add MousePosition control (#46)
    * Add MousePosition control
    
    * Add MousePosition control
    
    * Put comment son 2 lines
    
    * Typo

---
+++ src/components/MousePosition/MousePosition.js
@@ -0,0 +1,139 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import OLMap from 'ol/Map';
+import { createStringXY } from 'ol/coordinate';
+import OLMousePosition from 'ol/control/MousePosition';
+import Select from '../Select';
+
+const propTypes = {
+  /**
+   * The current map.
+   */
+  map: PropTypes.instanceOf(OLMap).isRequired,
+
+  /**
+   * CSS class of the checkbox.
+   */
+  className: PropTypes.string,
+
+  /**
+   * List of projections to display.
+   */
+  projections: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * The label to display in the select box.
+       */
+      label: PropTypes.string.isRequired,
+
+      /**
+       * The value used to create the options´s projection of the MousePosition control.
+       * See [doc](https://openlayers.org/en/latest/apidoc/module-ol_control_MousePosition.html).
+       */
+      value: PropTypes.string.isRequired,
+
+      /**
+       * A function following the  [CoordinateFormat](https://openlayers.org/en/latest/apidoc/module-ol_coordinate.html#~CoordinateFormat).
+       */
+      format: PropTypes.func,
+    }),
+  ),
+};
+
+const defaultProps = {
+  className: 'tm-mouse-position',
+  projections: [
+    {
+      label: 'EPSG:4326',
+      value: 'EPSG:4326',
+    },
+    {
+      label: 'EPSG:3857',
+      value: 'EPSG:3857',
+    },
+  ],
+};
+
+class MousePosition extends PureComponent {
+  constructor(props) {
+    super(props);
+    const { projections } = this.props;
+    this.state = {
+      projection: projections && projections[0],
+    };
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    this.updateControl();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { projection } = this.state;
+
+    if (prevState.projection !== projection) {
+      this.updateControl();
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.control) {
+      const { map } = this.props;
+      map.removeControl(this.control);
+    }
+  }
+
+  updateControl() {
+    const { map } = this.props;
+    const { projection } = this.state;
+
+    if (this.control) {
+      map.removeControl(this.control);
+    }
+
+    if (!projection || !this.ref || !this.ref.current) {
+      return;
+    }
+
+    this.control = new OLMousePosition({
+      coordinateFormat: projection.format || createStringXY(4),
+      projection: projection.value,
+      target: this.ref.current,
+      undefinedHTML: '&nbsp;',
+    });
+    map.addControl(this.control);
+  }
+
+  renderSelect() {
+    const { projections } = this.props;
+    if (!projections.length) {
+      return null;
+    }
+
+    return (
+      <Select
+        options={projections}
+        onChange={(evt, val) => {
+          this.setState({
+            projection: projections.find(p => p.value === val),
+          });
+        }}
+      />
+    );
+  }
+
+  render() {
+    const { className } = this.props;
+    return (
+      <div className={className}>
+        {this.renderSelect()}
+        <span ref={this.ref} />
+      </div>
+    );
+  }
+}
+
+MousePosition.propTypes = propTypes;
+MousePosition.defaultProps = defaultProps;
+
+export default MousePosition;
---
+++ src/components/MousePosition/MousePosition.test.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-canvas-mock';
+import OLMap from 'ol/Map';
+import OLMousePosition from 'ol/control/MousePosition';
+import MousePosition from './MousePosition';
+
+configure({ adapter: new Adapter() });
+const expectSnapshot = props => {
+  const map = new OLMap({});
+  const component = renderer.create(<MousePosition map={map} {...props} />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+};
+
+describe('MousePosition', () => {
+  describe('matches snapshots', () => {
+    test('using default values.', () => {
+      expectSnapshot({});
+    });
+
+    test('using no projections.', () => {
+      expectSnapshot({
+        projections: [],
+      });
+    });
+
+    test('using only one projection', () => {
+      expectSnapshot({
+        projections: [{ label: 'foo', value: 'foo' }],
+      });
+    });
+
+    test('using multiple projections.', () => {
+      expectSnapshot({
+        projections: [
+          { label: 'foo', value: 'foo' },
+          { label: 'bar', value: 'bar' },
+        ],
+      });
+    });
+  });
+
+  test('add MousePosition control to the map.', () => {
+    const map = new OLMap({});
+    const spy = jest.spyOn(map, 'removeControl');
+    const spy2 = jest.spyOn(map, 'addControl');
+    const fn = jest.fn();
+    const wrapper = mount(
+      <MousePosition
+        map={map}
+        projections={[
+          {
+            label: 'EPSG:4326',
+            value: 'EPSG:4326',
+            format: fn,
+          },
+        ]}
+      />,
+    );
+    const ctrl = wrapper.instance().control;
+    expect(spy).toHaveBeenCalledTimes(0);
+    expect(spy2).toHaveBeenCalledTimes(1);
+    expect(spy2.mock.calls[0][0]).toBeInstanceOf(OLMousePosition);
+    expect(spy2.mock.calls[0][0]).toBe(ctrl);
+    expect(ctrl.getProjection().getCode()).toBe('EPSG:4326');
+    expect(ctrl.getCoordinateFormat()).toBe(fn);
+  });
+
+  test('remove MousePosition control before adding it again.', () => {
+    const map = new OLMap({});
+    const spy = jest.spyOn(map, 'removeControl');
+    const spy2 = jest.spyOn(map, 'addControl');
+    const wrapper = mount(<MousePosition map={map} />);
+    expect(spy).toHaveBeenCalledTimes(0);
+    expect(spy2).toHaveBeenCalledTimes(1);
+    wrapper.setState({
+      projection: wrapper.props().projections[1],
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy.mock.calls[0][0]).toBe(spy2.mock.calls[0][0]);
+    expect(spy2).toHaveBeenCalledTimes(2);
+    expect(spy2.mock.calls[1][0]).toBe(wrapper.instance().control);
+    expect(spy2.mock.calls[1][0]).toBeInstanceOf(OLMousePosition);
+  });
+
+  test('remove MousePosition control on unmount.', () => {
+    const map = new OLMap({});
+    const spy = jest.spyOn(map, 'removeControl');
+    const spy2 = jest.spyOn(map, 'addControl');
+    const wrapper = mount(<MousePosition map={map} />);
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.unmount();
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy.mock.calls[0][0]).toBe(spy2.mock.calls[0][0]);
+  });
+});
