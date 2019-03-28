05671e80cdde227ede4d123802052126ca516aa9
danji90 <danji_ma90@hotmail.com>
2019-03-28 17:04:19 +0100

    modal working, still missing disable content

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,42 +1,96 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { MDCDialog } from '@material/dialog';
+import './Dialog.scss';
 
 const propTypes = {
   title: PropTypes.string,
   children: PropTypes.node,
-  className: PropTypes.string,
-  isModal: PropTypes.Boolean,
-  isOpen: PropTypes.Boolean,
+  // className: PropTypes.string,
 };
 
 const defaultProps = {
   title: 'A Dialog Box',
   children: undefined,
-  className: 'mdc-dialog',
-  isModal: false,
-  isOpen: true,
+  // className: 'tm-dialog',
 };
 
 class Dialog extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isModal: false,
+      isOpen: true,
+    };
+    this.openDialogHandler = this.openDialogHandler.bind(this);
+    this.closeDialogHandler = this.closeDialogHandler.bind(this);
+  }
 
-    this.dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+  openDialogHandler() {
+    this.setState(
+      () => ({
+        isOpen: true,
+        isModal: true,
+      }),
+      () => {
+        console.log('Dialog opened');
+        console.log(this.state);
+      },
+    );
+    const child = document.getElementById('child');
+    const parent = document.getElementById('parent');
+    parent.classList.add('disabled')
+    child.classList.remove('disabled');
+  }
+
+  closeDialogHandler() {
+    this.setState(
+      () => ({
+        isOpen: false,
+        isModal: false,
+      }),
+      () => {
+        console.log('Dialog closed');
+        console.log(this.state);
+      },
+    );
+    const element = document.getElementById('parent');
+    element.classList.add('disabled');
   }
 
   render() {
-    const { title, children, className, isModal, isOpen } = this.props;
+    const { title, children } = this.props;
+    const { isOpen } = this.state;
     return (
-      <div
-        className={className}
-        title={title}
-        isModal={isModal}
-        isOpen={isOpen}
-      >
-        <h1>{title}</h1>
-        {children}
-      </div>
+      <React.Fragment>
+        <div id="parent">
+          <button
+            type="button"
+            className="tm-dialog-btn"
+            onClick={this.openDialogHandler}
+          >
+            Open Dialog
+          </button>
+          <div id="child" title={title}>
+            <div
+              className="modal-wrapper"
+              style={{
+                transform: isOpen ? 'translateY(0vh)' : 'translateY(-100vh)',
+                opacity: isOpen ? '1' : '0',
+              }}
+            >
+              <h1>{title}</h1>
+              <div className="modal-body">{children}</div>
+              <button
+                type="button"
+                className="tm-dialog-btn"
+                onClick={this.closeDialogHandler}
+              >
+                Close Dialog
+              </button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/Dialog/README.md
+++ src/components/Dialog/README.md
@@ -6,10 +6,9 @@ This demonstrates the use of Dialog.
 import React from 'react';
 import Dialog from 'react-spatial/components/Dialog';
 
-<Dialog className='tm-dialog-example' title='Platypus'>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
-  <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am</p>
-</Dialog>;
+<Dialog modal title="Beispieldialog">
+  <span>I am the content of the dialog</span>
+</Dialog>
 
 export default Dialog;
 ```
