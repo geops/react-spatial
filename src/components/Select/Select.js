1d433685f6159c9cb666a40598ff6b29712dcca9
Olivier Terral <olivier.terral@geops.de>
2019-04-02 17:34:15 +0200

    Makes the Select more useable

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -11,16 +11,35 @@ const propTypes = {
    * Array of values to use in the select element.
    */
   options: PropTypes.arrayOf(
-    PropTypes.shape({
-      value: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.shape({
+        value: PropTypes.oneOfType([
+          PropTypes.string,
+          PropTypes.number,
+          PropTypes.arrayOf(PropTypes.string),
+        ]).isRequired,
+        label: PropTypes.string.isRequired,
+      }),
+    ]),
+  ),
 
   /**
    * Selected option object.
    */
-  value: PropTypes.string,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.shape({
+      value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.arrayOf(PropTypes.string),
+      ]).isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ]),
 
   /**
    * Function triggered on select's change event.
@@ -31,7 +50,25 @@ const propTypes = {
 
 const defaultProps = {
   value: undefined,
+  options: undefined,
   className: 'tm-select',
+};
+
+const getSelectedOption = (inputValue, options) => {
+  return options.find(
+    opt =>
+      (typeof opt === 'string' && opt === inputValue) ||
+      opt.value === inputValue ||
+      (opt.value && opt.value.toString && opt.value.toString() === inputValue),
+  );
+};
+
+const getValue = opt => {
+  return typeof opt === 'string' ? opt : opt.value;
+};
+
+const getLabel = opt => {
+  return typeof opt === 'string' ? opt : opt.label;
 };
 
 /**
@@ -39,15 +76,22 @@ const defaultProps = {
  */
 const Select = props => {
   const { value, className, options, onChange } = props;
+  if (!options) {
+    return null;
+  }
+  const inputValue = getValue(value);
+
   return (
     <select
       className={className}
-      value={value}
-      onChange={evt => onChange(evt, evt.target.value)}
+      value={inputValue}
+      onChange={evt => {
+        onChange(evt, getSelectedOption(evt.target.value, options));
+      }}
     >
       {(options || []).map(o => (
-        <option key={o.value} value={o.value}>
-          {o.label}
+        <option key={getValue(o)} value={getValue(o)}>
+          {getLabel(o)}
         </option>
       ))}
     </select>
