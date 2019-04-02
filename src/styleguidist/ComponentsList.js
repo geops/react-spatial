a0328de96f5cac10597ee351b5825c451f5a2618
edellucien <edellucien@gmail.com>
2019-04-02 15:15:42 +0200

    Overwrite styleguidist 'ComponentsList' to allor href to fit with Per… (#60)
    * Overwrite styleguidist 'ComponentsList' to allor href to fit with Permalink behavior
    
    * Set default props for ComponentsList Styleguidist replacement
    
    * If item.href defined use it, else getUrl method, without leading slash
    
    * Lint fix
    
    * More explicit comment for replacement

---
+++ src/styleguidist/ComponentsList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// Import default implementation from react-styleguidist using the full path
+import ComponentsListRenderer from 'react-styleguidist/lib/client/rsg-components/ComponentsList/ComponentsListRenderer';
+import getUrl from 'react-styleguidist/lib/client/utils/getUrl';
+
+const propTypes = {
+  items: PropTypes.array.isRequired,
+  classes: PropTypes.object,
+  hashPath: PropTypes.array,
+  useRouterLinks: PropTypes.bool,
+  useHashId: PropTypes.bool,
+};
+
+const defaultProps = {
+  classes: null,
+  hashPath: [],
+  useRouterLinks: false,
+  useHashId: true,
+};
+
+function ComponentsList({
+  classes,
+  items,
+  useRouterLinks = false,
+  useHashId,
+  hashPath,
+}) {
+  const mappedItems = items.map(item => ({
+    ...item,
+    href: item.href
+      ? item.href
+      : // Conflict with Permalink Component: Remove the first '/' to avoid page reload on click
+        getUrl({
+          name: item.name,
+          slug: item.slug,
+          anchor: !useRouterLinks,
+          hashPath: useRouterLinks ? hashPath : false,
+          id: useRouterLinks ? useHashId : false,
+        }).replace(/^\/+/g, ''),
+  }));
+  return <ComponentsListRenderer classes={classes} items={mappedItems} />;
+}
+
+ComponentsList.propTypes = propTypes;
+ComponentsList.defaultProps = defaultProps;
+
+export default ComponentsList;
