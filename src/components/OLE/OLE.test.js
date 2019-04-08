d5a856c8dc2b13c5cb247718c761e652990cbfa0
Olivier Terral <olivier.terral@geops.de>
2019-04-08 09:31:29 +0200

    Add very basic test for ole

--- src/components/OLE/OLE.test.js
+++ src/components/OLE/OLE.test.js
@@ -1,6 +1,6 @@
 import 'jest-canvas-mock';
 import React from 'react';
-import { configure } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import VectorSource from 'ol/source/Vector';
@@ -22,10 +22,56 @@ describe('OLE', () => {
   beforeEach(() => {
     map.setTarget(document.createElement('div'));
   });
+
   test('matches snapshots', () => {
     window.ole = 'fgjkfgio';
     const component = renderer.create(<OLE map={map} layer={vectorLayer} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('creates Editor with all controls with no error', () => {
+    window.ole = 'fgjkfgio';
+    mount(
+      <OLE
+        map={map}
+        layer={vectorLayer}
+        cad
+        drawPoint
+        drawCustoms={[{}]}
+        drawLineString
+        drawPolygon
+        move
+        rotate
+        modify
+        del
+        buffer
+        union
+        intersection
+        difference
+      />,
+    );
+  });
+
+  test('creates Editor without controls with no error', () => {
+    window.ole = 'fgjkfgio';
+    mount(
+      <OLE
+        map={map}
+        layer={vectorLayer}
+        cad={false}
+        drawPoint={false}
+        drawLineString={false}
+        drawPolygon={false}
+        move={false}
+        rotate={false}
+        modify={false}
+        del={false}
+        buffer={false}
+        union={false}
+        intersection={false}
+        difference={false}
+      />,
+    );
   });
 });
