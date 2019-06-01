eb1fb32ec340a1ff63ca972d6d9570af454b7719
angsherpa232 <angsherpa232@gmail.com>
2019-06-01 10:56:07 +0200

    sidemenu bar with new design

--- src/components/Sidebar/README.md
+++ src/components/Sidebar/README.md
@@ -15,7 +15,7 @@ import { getCenter} from 'ol/extent';
 import OLMap from 'ol/Map';
 import Sidebar from 'react-spatial/components/Sidebar';
 import SidebarItem from 'react-spatial/components/SidebarItem';
-
+import Header from 'react-spatial/components/Header';
 
 
 class SidebarExample extends React.Component {
@@ -83,27 +83,9 @@ class SidebarExample extends React.Component {
     return (
       <div className="tm-sidebar-example" 
       onClick={()=>this.toggleSidebar()}>
+      
    <BasicMap center={this.center} zoom={17} layers={this.layers} />
-        <Sidebar className={`${this.state.open ? 'tm-sidebar' : 'tm-sidebar-closed'}`}
-        >
-
-        <div className="tm-sidebar-content">
-        {!this.state.showIconOnly ? 
-          <SidebarItem title="Sidebar" iconOnly={this.state.showIconOnly}>
-           <p onClick={()=>this.callA()}>Alpha</p>
-           <p onClick={()=>this.callB()}>Beta</p>
-           <p onClick={()=>this.callC()}>Cheta</p>
-           </SidebarItem>
-           :
-           <SidebarItem title="Sidebar" iconOnly={this.state.showIconOnly}>
-           <p onClick={()=>this.callA()}>A</p>
-           <p  onClick={()=>this.callB()}>B</p>
-           <p  onClick={()=>this.callC()}>C</p>
-           </SidebarItem>
-        }
-
-        </div>
-        </Sidebar> 
+      <Sidebar /> 
       </div>
     );
   }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,6 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+// const propTypes = {
+//   title: PropTypes.string,
+//   children: PropTypes.array.isRequired,
+//   className: PropTypes.string,
+//   closedClassName:  PropTypes.string,
+//   classNameContent: PropTypes.string,
+//   showIconOnly: PropTypes.bool
+// };
+
+// const defaultProps = {
+//   className: 'tm-sidebar',
+//   classNameContent: 'tm-sidebar-content'
+// };
+
+// const Sidebar = ({ children, className, classNameContent}) => (
+//   <div className={className} showIconOnly>
+//     <div className={classNameContent}>{children}</div>
+//   </div>
+// );
+
 const propTypes = {
   title: PropTypes.string,
   children: PropTypes.array.isRequired,
@@ -16,11 +36,26 @@ const defaultProps = {
   classNameContent: 'tm-sidebar-content'
 };
 
-const Sidebar = ({ children, className, classNameContent}) => (
-  <div className={className} showIconOnly>
-    <div className={classNameContent}>{children}</div>
-  </div>
-);
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+      let classes = ['tm-sidebar'];
+      if (true) {
+          classes = ['tm-sidebar','Open']
+      }
+  return (
+      <nav className={classes.join(' ')}>
+          <ul>
+              <li><a href="/">Save map</a></li>
+              <li><a href="/">Zoom</a></li>
+          </ul>
+      </nav>
+  );
+  }
+};
 
 Sidebar.propTypes = propTypes;
 Sidebar.defaultProps = defaultProps; 
