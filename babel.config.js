3d1f9a4273d4f77f41218d39e64d85dc7663b4df
Olivier Terral <olivier.terral@geops.de>
2019-06-28 11:35:09 +0200

    Make async work in tests

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-runtime'], // for async/await
+    },
+  },
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
