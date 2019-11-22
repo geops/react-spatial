03bbfc552f835ba03e02ecc0e045d97a351fec63
Olivier Terral <olivier.terral@geops.de>
2019-11-22 10:15:30 +0100

    Fix NorthArrow tests

--- src/components/NorthArrow/NorthArrow.test.js
+++ src/components/NorthArrow/NorthArrow.test.js
@@ -1,7 +1,8 @@
 import 'jest-canvas-mock';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { configure, shallow } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import MapEvent from 'ol/MapEvent';
 import OLMap from 'ol/Map';
@@ -10,63 +11,97 @@ import { TiImage } from 'react-icons/ti';
 import NorthArrow from './NorthArrow';
 
 configure({ adapter: new Adapter() });
+let olView;
+let olMap;
 
 describe('NorthArrow', () => {
-  const olView = new OLView();
-  const olMap = new OLMap({ view: olView });
+  beforeEach(() => {
+    olView = new OLView();
+    olMap = new OLMap({ view: olView });
+  });
 
-  test('should match snapshot simple.', () => {
-    const component = renderer.create(
-      <NorthArrow map={olMap} className="test-class" />,
-    );
+  test('should match snapshot with default value.', () => {
+    let component;
+    renderer.act(() => {
+      component = renderer.create(<NorthArrow map={olMap} />);
+    });
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should match snapshot with custom attributes.', () => {
+    let component;
+    renderer.act(() => {
+      component = renderer.create(
+        <NorthArrow map={olMap} className="test-class" tabIndex={0} />,
+      );
+    });
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test('should match snapshot with children.', () => {
-    const component = renderer.create(
-      <NorthArrow map={olMap}>
-        <TiImage />
-      </NorthArrow>,
-    );
+    let component;
+    renderer.act(() => {
+      component = renderer.create(
+        <NorthArrow map={olMap}>
+          <TiImage />
+        </NorthArrow>,
+      );
+    });
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test('should match snapshot rotated.', () => {
-    const component = renderer.create(
-      <NorthArrow map={olMap} rotationOffset={45} />,
-    );
+    let component;
+    renderer.act(() => {
+      component = renderer.create(
+        <NorthArrow map={olMap} rotationOffset={45} />,
+      );
+    });
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test('should match snapshot with circle.', () => {
-    const component = renderer.create(<NorthArrow circled map={olMap} />);
+    let component;
+    renderer.act(() => {
+      component = renderer.create(<NorthArrow circled map={olMap} />);
+    });
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  test('should react on view rotation.', () => {
-    const wrapper = shallow(<NorthArrow map={olMap} />);
-    const spy = jest.spyOn(wrapper.instance(), 'onRotate');
+  test('should react on view rotation (transform: `rotate(20deg)`)', () => {
+    const wrapper = mount(<NorthArrow map={olMap} />);
     // Trigger view rotation
     olMap.getView().setRotation(0.3490658503988659);
-    olMap.dispatchEvent(new MapEvent('postrender', olMap));
-
-    expect(spy).toHaveBeenCalledTimes(1);
-    // 20 degrees = 0.3490658503988659 radians
-    expect(wrapper.instance().state.rotation).toBe(20);
+    act(() => {
+      olMap.dispatchEvent(new MapEvent('postrender', olMap));
+      // 20 degrees = 0.3490658503988659 radians
+    });
+    expect(wrapper.html()).toMatchSnapshot();
   });
 
-  test('should react on view rotation with offset.', () => {
-    const wrapper = shallow(<NorthArrow map={olMap} rotationOffset={-10} />);
-    const spy = jest.spyOn(wrapper.instance(), 'onRotate');
-
+  test('should react on view rotation with offset (transform: `rotate(10deg)`)', () => {
+    const wrapper = mount(<NorthArrow map={olMap} rotationOffset={-10} />);
     olMap.getView().setRotation(0.3490658503988659);
-    olMap.dispatchEvent(new MapEvent('postrender', olMap));
+    act(() => {
+      olMap.dispatchEvent(new MapEvent('postrender', olMap));
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(wrapper.instance().state.rotation).toBe(10);
+  test('should remove post render event on unmount', () => {
+    const wrapper = mount(<NorthArrow map={olMap} rotationOffset={-10} />);
+    act(() => {
+      olMap.dispatchEvent(new MapEvent('postrender', olMap));
+    });
+    // eslint-disable-next-line no-underscore-dangle
+    expect(olMap.listeners_.postrender.length).toBe(3);
+    wrapper.unmount();
+    // eslint-disable-next-line no-underscore-dangle
+    expect(olMap.listeners_.postrender.length).toBe(2);
   });
 });
--- src/components/NorthArrow/README.md
+++ src/components/NorthArrow/README.md
@@ -17,64 +17,50 @@ import TileImageSource from 'ol/source/TileImage';
 import { getCenter} from 'ol/extent';
 import OLMap from 'ol/Map';
 
-class NorthArrowExample extends React.Component {
-  constructor(props) {
-    super(props);
+const extent = [599500, 199309, 600714, 200002];
+const resolutions = [
+  6.927661,
+  3.4638305,
+  1.73191525,
+  0.865957625,
+  0.4329788125,
+  0.21648940625,
+  0.108244703125,
+];
 
-    const extent = [599500, 199309, 600714, 200002];
-    const resolutions = [
-      6.927661,
-      3.4638305,
-      1.73191525,
-      0.865957625,
-      0.4329788125,
-      0.21648940625,
-      0.108244703125,
-    ];
-
-    const layer = new Layer({
-      name: 'Layer',
-      olLayer: new TileLayer({
-        extent,
-        source: new TileImageSource({
-          tileUrlFunction: c =>
-            '//plans.trafimage.ch/static/tiles/' +
-            `bern_aussenplan/${c[0]}/${c[1]}/${-c[2]-1}.png`,
-          tileGrid: new TileGrid({
-            origin: [extent[0], extent[1]],
-            resolutions,
-          }),
-        }),
+const layer = new Layer({
+  name: 'Layer',
+  olLayer: new TileLayer({
+    extent,
+    source: new TileImageSource({
+      tileUrlFunction: c =>
+        '//plans.trafimage.ch/static/tiles/' +
+        `bern_aussenplan/${c[0]}/${c[1]}/${-c[2]-1}.png`,
+      tileGrid: new TileGrid({
+        origin: [extent[0], extent[1]],
+        resolutions,
       }),
-    });
-    this.center = getCenter(extent);
-    this.layers = [layer];
+    }),
+  }),
+});
+const center = getCenter(extent);
+const layers = [layer];
+const map = new OLMap({
+  controls: [],
+  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+});
 
-    this.map = new OLMap({
-      controls: [],
-      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
-    });
-  }
-
-  render() {
-    return (
-      <div className="tm-north-arrow-example">
-        <BasicMap
-          map={this.map}
-          center={this.center}
-          zoom={17}
-          layers={this.layers}
-        />
-        <NorthArrow
-          className="tm-north-arrow"
-          rotationOffset={20}
-          map={this.map}
-          circled
-        />
-      </div>
-    );
-  }
-}
-
-<NorthArrowExample />;
+<div style={{position:'relative'}}>
+  <BasicMap
+    map={map}
+    center={center}
+    zoom={17}
+    layers={layers}
+  />
+  <NorthArrow
+    map={map}
+    rotationOffset={20}
+    circled
+  />
+</div>;
 ```
