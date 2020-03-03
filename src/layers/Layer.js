74bf9ba20e73aebb2ec971772de7f3c960c29701
Mario Härtwig <mario.haertwig@geops.de>
2020-03-03 17:22:43 +0100

    Add layer property for duck typing.

--- src/layers/Layer.js
+++ src/layers/Layer.js
@@ -44,6 +44,10 @@ export default class Layer extends Observable {
     this.properties = properties || {};
     this.isQueryable = isQueryable !== false;
 
+    // Custom property for duck typing since `instanceof` is not working
+    // when the instance was created on different bundles.
+    this.isReactSpatialLayer = true;
+
     // This array contains openlayers listeners keys to unlisten in terminate function.
     this.olListenersKeys = [];
 
