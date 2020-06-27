18d3f8a71ac98ded221d1c474eb4464f2d68c1e1
David Weber <weber.aulendorf@googlemail.com>
2020-06-27 14:22:40 +0200

    fix setting rotation to 0 in MapBox Layer

--- src/layers/MapboxLayer.js
+++ src/layers/MapboxLayer.js
@@ -58,7 +58,7 @@ export default class MapboxLayer extends Layer {
 
         // adjust view parameters in mapbox
         const { rotation } = viewState;
-        if (rotation && this.renderState.rotation !== rotation) {
+        if (rotation != null && this.renderState.rotation !== rotation) {
           this.mbMap.rotateTo((-rotation * 180) / Math.PI, {
             animate: false,
           });
