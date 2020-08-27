01a644a7de1c431831e7419e47ce2bcaa40e9182
Daniel <daniel.marsh-hunn@geops.de>
2020-08-27 14:23:30 +0200

    added test

--- src/components/BaseLayerSwitcher/BaseLayerSwitcher.test.js
+++ src/components/BaseLayerSwitcher/BaseLayerSwitcher.test.js
@@ -40,17 +40,25 @@ describe('BaseLayerSwitcher', () => {
   });
 
   test('the correct baselayer is visible on mount', () => {
-    const comp = mountComp(data);
+    const comp = mountComp();
     expect(comp.props().layers[0].visible).toBe(true);
   });
 
   test('removes open class and switches layer on click', () => {
-    const comp = mountComp(data);
+    const comp = mountComp();
     comp.find('.rs-opener').at(0).simulate('click');
     comp.find('.rs-base-layer-switcher-button').at(3).simulate('click');
     expect(
       comp.props().layers.filter((layer) => layer.isBaseLayer)[2].visible,
     ).toBe(true);
     expect(comp.find('.rs-base-layer-switcher rs-open').exists()).toBe(false);
+  });
+
+  test.only('toggles base map instead of opening when only two base layers', () => {
+    const comp = mountComp(data.slice(0, 2));
+    comp.find('.rs-opener').at(0).simulate('click');
+    expect(
+      comp.props().layers.filter((layer) => layer.isBaseLayer)[1].visible,
+    ).toBe(true);
   });
 });
