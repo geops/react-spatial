21730f5b5a09452145538c3d5b130b711e00a3e3
Daniel <daniel.marsh-hunn@geops.de>
2020-11-10 15:21:29 +0100

    seting Search input value on keyboard Enter

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -121,6 +121,7 @@ function Search({
                 const filtered = suggestions.filter((s) => s.items.length > 0);
                 if (filtered.length > 0) {
                   const { items, section } = filtered[0];
+                  setValue(items[0].properties.name);
                   onSelect({ ...items[0], section });
                 }
               } else if (
