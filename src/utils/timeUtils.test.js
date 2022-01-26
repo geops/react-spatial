3ffb5dcc54c3ef500742c753224b244e7071382f
Olivier Terral <olivier.terral@geops.de>
2022-01-26 17:24:38 +0100

    Add utils function

--- src/utils/timeUtils.test.js
+++ src/utils/timeUtils.test.js
@@ -1,8 +1,27 @@
 import { getHoursAndMinutes, getDelayString } from './timeUtils';
 
+const RealDate = Date;
 describe('timeUtils', () => {
+  beforeEach(() => {
+    global.Date = jest.fn(() => {
+      return {
+        getHours: () => {
+          return 0;
+        },
+        getMinutes: () => {
+          return 2;
+        },
+      };
+    });
+    Object.assign(Date, RealDate);
+  });
+
+  afterEach(() => {
+    global.Date = RealDate;
+  });
+
   test('getHoursAndMinutes should be correct.', () => {
-    expect(getHoursAndMinutes(123456)).toBe('01:02');
+    expect(getHoursAndMinutes(123456)).toBe('00:02');
   });
 
   test('getDelayString should be correct.', () => {
