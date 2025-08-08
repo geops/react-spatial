b30824121f71bf1f7b8984f39654be4e3df22ea7
Olivier Terral <olivier.terral@geops.com>
2025-08-08 17:06:28 +0200

    test: remove enzyme

--- src/components/MousePosition/MousePosition.test.js
+++ src/components/MousePosition/MousePosition.test.js
@@ -1,20 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-import { configure, mount } from "enzyme";
+import { fireEvent, render } from "@testing-library/react";
 import "jest-canvas-mock";
 import OLMousePosition from "ol/control/MousePosition";
 import OLMap from "ol/Map";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import MousePosition from "./MousePosition";
 
-configure({ adapter: new Adapter() });
 const expectSnapshot = (props) => {
   const map = new OLMap({});
-  const component = renderer.create(<MousePosition map={map} {...props} />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container } = render(<MousePosition map={map} {...props} />);
+  expect(container.innerHTML).toMatchSnapshot();
 };
 
 describe("MousePosition", () => {
@@ -50,7 +46,7 @@ describe("MousePosition", () => {
     const spy = jest.spyOn(map, "removeControl");
     const spy2 = jest.spyOn(map, "addControl");
     const fn = jest.fn();
-    mount(
+    render(
       <MousePosition
         map={map}
         projections={[
@@ -74,10 +70,10 @@ describe("MousePosition", () => {
     const map = new OLMap({});
     const spy = jest.spyOn(map, "removeControl");
     const spy2 = jest.spyOn(map, "addControl");
-    const wrapper = mount(<MousePosition map={map} />);
+    const { unmount } = render(<MousePosition map={map} />);
     expect(spy).toHaveBeenCalledTimes(0);
     expect(spy2).toHaveBeenCalledTimes(1);
-    wrapper.unmount();
+    unmount();
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy.mock.calls[0][0]).toBe(spy2.mock.calls[0][0]);
   });
@@ -85,7 +81,7 @@ describe("MousePosition", () => {
   test("triggers onChange when select projection.", () => {
     const map = new OLMap({});
     const onChange = jest.fn(() => {});
-    const wrapper = mount(
+    const { container } = render(
       <MousePosition
         map={map}
         onChange={onChange}
@@ -100,7 +96,7 @@ describe("MousePosition", () => {
     );
     // onChange triggered on instantiation.
     expect(onChange).toHaveBeenCalledTimes(0);
-    wrapper.find("select").simulate("change", {});
+    fireEvent.change(container.querySelector("select"), {});
     expect(onChange).toHaveBeenCalledTimes(1);
   });
 
@@ -119,14 +115,17 @@ describe("MousePosition", () => {
         value: "EPSG:3857",
       },
     ];
-    const wrapper = mount(<MousePosition map={map} projections={projs} />);
+    const { container } = render(
+      <MousePosition map={map} projections={projs} />,
+    );
 
     const ctrl = spy.mock.calls[0][0];
     expect(ctrl.getProjection().getCode()).toBe(projs[0].value);
     expect(ctrl.getCoordinateFormat()).toBe(projs[0].format);
-    wrapper
-      .find("select")
-      .simulate("change", { target: { value: "EPSG:3857" } });
+
+    fireEvent.change(container.querySelector("select"), {
+      target: { value: "EPSG:3857" },
+    });
     expect(ctrl.getProjection().getCode()).toBe(projs[1].value);
     expect(ctrl.getCoordinateFormat()).toBe(projs[1].format);
   });
