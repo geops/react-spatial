5702c616a3e5b1417d65029145e4c21707cbf588
danji90 <daniel.marsh-hunn@geops.com>
2026-02-09 13:35:15 +0100

    chore: fix setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
 import ResizeObserver from "resize-observer-polyfill";
 
+// eslint-disable-next-line perfectionist/sort-imports
+const util = require("util");
+
+if (!global.TextDecoder) {
+  global.TextDecoder = util.TextDecoder;
+}
+
+if (!global.TextEncoder) {
+  global.TextEncoder = util.TextEncoder;
+}
+
 global.URL.createObjectURL = jest.fn(() => {
   return "fooblob";
 });
@@ -23,21 +34,14 @@ class Worker {
   }
 }
 
-Object.defineProperty(window, "Worker", {
-  value: Worker,
-  writable: true,
-});
+Object.defineProperty(window, "Worker", { value: Worker, writable: true });
 
 const mock = {
   clearWatch: jest.fn(),
   getCurrentPosition: jest.fn(),
   watchPosition: (onSuccess) => {
     onSuccess({
-      coords: {
-        accuracy: 55,
-        latitude: 47.9913611,
-        longitude: 7.84868,
-      },
+      coords: { accuracy: 55, latitude: 47.9913611, longitude: 7.84868 },
       timestamp: 1552660077044,
     });
   },
