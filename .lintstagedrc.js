f70e52bf1ed48a06225443a784160830e08255e2
danji90 <daniel.marsh-hunn@geops.com>
2026-02-09 13:35:33 +0100

    chore: fix setupTests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "(src|__mocks__)/**/*.js": [
+  "(src|__mocks__)/**/!(*setupTests).js": [
     "eslint --fix",
     "prettier --write",
-    "yarn test --bail --findRelatedTests",
+    "jest --bail --findRelatedTests",
   ],
   "package.json": ["fixpack"],
   "src/**/*.{css,scss}": ["stylelint --fix"],
