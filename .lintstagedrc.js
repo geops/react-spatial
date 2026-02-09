5e60da82ad13a54da0b16fce4b3bb590703901b1
danji90 <daniel.marsh-hunn@geops.com>
2026-02-09 13:37:35 +0100

    chore: fix setupTests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-  // Always lint & format source files
-  "(src|__mocks__)/**/*.js": ["eslint --fix", "prettier --write"],
-
-  // Only run Jest when actual test files are staged
-  "(src|__mocks__)/**/*.{test,spec}.js": ["jest --bail --findRelatedTests"],
-
+  "(src|__mocks__)/**/*.js": [
+    "eslint --fix",
+    "prettier --write",
+    "yarn test --bail --findRelatedTests",
+  ],
   "package.json": ["fixpack"],
   "src/**/*.{css,scss}": ["stylelint --fix"],
 };
